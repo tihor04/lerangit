b786e9020750b355076844b599e2ce34ed04ac95
Rohit singh <srrohitsingh1738@gmail.com>
2021-11-13 20:27:01 +0530

    initial commit

---
+++ add.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+void solve()
+{
+   int a=5;
+   int b=11;
+   cout<<a+b;
+}
+int main()
+{
+
+   
+
+
+  
+
+
+
+  solve();
+
+ return 0;
+}
