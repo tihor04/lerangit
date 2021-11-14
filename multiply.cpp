e83eeb65f900114f5258dd9cc658c129eb189a55
Rohit singh <srrohitsingh1738@gmail.com>
2021-11-14 11:59:28 +0530

    added multiply function

---
+++ multiply.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+void solve()
+{
+   int a =3;
+   int b=3;
+   cout<<a*b;
+}
+int main()
+{
+
+  solve();
+
+ return 0;
+}
